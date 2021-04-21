ecb7f4eabf3070e70d167cf953bfa2747297c18e
Colclough7 <you@example.com>
2021-04-21 07:51:50 -0400

    fix fetch request in code

--- index.js
+++ index.js
@@ -6,19 +6,16 @@ const rate = document.querySelector('#rate')
 const swap = document.querySelector('#swap')
 
 //Fetch exchange rates and update the DOM 
-const calculate = async()=>{
+const calculate = ()=>{
     const one = currencyOne.value
     const two = currencyTwo.value
-    try {
-    const response = await fetch(`https://v6.exchangerate-api.com/v6/36fe98b4ac6c41315f653ada/latest/${one}`)
-    const data = await response.json()
-    const {conversion_rates} = data 
-    const currancyRate = conversion_rates[two]
-    rate.innerHTML = `1 ${one} = ${currancyRate} ${two}`
-    amountTwo.value = (amountOne.value * currancyRate).toFixed(2)
-    } catch (error) {
-    console.log(error)   
-    }  
+    fetch(`https://v6.exchangerate-api.com/v6/36fe98b4ac6c41315f653ada/latest/${one}`)
+    .then(res => res.json())
+    .then(data => {
+        const rates = data.conversion_rates[two]
+        rate.innerText = `1 ${one} = ${rates} ${two}`
+        amountTwo.value = (amountOne.value * rates).toFixed(2)
+    })
 }
 //Swap Handler
 const swapHandler = ()=>{
